7fb35ebfc651e52721ab2b5c35a0a700df907d29
madechun <45516694@qq.com>
2019-12-26 00:21:58 +0800

    add ctx

--- Koa/index.js
+++ Koa/index.js
@@ -1,9 +1,10 @@
 const Koa = require('./lib')
 const app = new Koa()
 
-app.use((req, res) => {
-	res.writeHead(200)
-	res.end('hello koa~~')
+app.use(ctx => {
+	ctx.body = 'hello koa'
+	// res.writeHead(200)
+	// res.end('hello koa~~')
 })
 
 app.listen(3001, _ => {
--- Koa/lib/index.js
+++ Koa/lib/index.js
@@ -1,9 +1,15 @@
 const http = require('http')
+const context = require('./context')
+const request = require('./request')
+const response = require('./response')
 
 class Koa {
 	listen(...args) {
 		const server = http.createServer((req, res) => {
-			this.callback(req, res)
+			// this.callback(req, res)
+			const ctx = this.createContext(req, res)
+			this.callback(ctx)
+			res.end(ctx.body)
 		})
 
 		server.listen(...args)
@@ -11,6 +17,16 @@ class Koa {
 	// 注册中间件
 	use(callback) {
 		this.callback = callback
+	}
+
+	createContext(req, res) {
+		const ctx = Object.create(context)
+		ctx.request = Object.create(request)
+		ctx.response = Object.create(response)
+
+		ctx.req = ctx.request.req = req
+		ctx.res = ctx.response.res = res
+		return ctx
 	}
 }
 
